fe35209cb0f32462996ecde7c28fdb73df9e845f
sebastianboyer <bazboyer@hotmail.com>
2021-12-06 20:53:41 -0500

    Just did 50 minutes for nothing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,21 +9,27 @@ public class Main {
         SpeedOfCar speed = new SpeedOfCar();
         Turning direction = new Turning();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Do you want to go for a drive? (Enter: true/false) ");
-        Boolean yn;
-        Boolean start = scanner.nextBoolean();
-
-            if (true) {
-                System.out.print("Lets go for a drive! ");
-            } else {
-                System.out.print("Come back when you want to go for a drive!");
-            }
-
-            System.out.println("Hello World");
-
-
+        System.out.print("Do you want to go for a drive?- ");
+        Scanner sc = new Scanner(System.in);
+        boolean bn = sc.nextBoolean();
+        if (bn == true) {
+            System.out.println("Lets go for a drive");
+        } else if (bn == false) {
+            System.out.println("Come back when you want to go for a drive");
+        }
+        sc.close();
+        /*System.out.println("Do you want to go for a drive? (Enter: yes/no) ");
+        Boolean yes = true;
+        Boolean no = false;
+        boolean bn = scanner.nextBoolean();
+        if (bn = true) {
+            System.out.println("Lets go for a drive!");
+        }else if (bn = false)
+            System.out.println("Come back when you want to go for a drive!");
+        }*/
 
 
     }
-}
+    }
+
+
