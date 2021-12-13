5e4d4c30b08df183a8c4d8b55a4cce0b8eeb9955
lillianbegley <lbegley1@umbc.edu>
2021-12-12 22:11:34 -0500

    Modified Version1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,14 +11,6 @@ public class Main {
         speed limit = new speed();
         KeyFob fob = new KeyFob();
 
-
-//      System.out.print("Do you want to go for a drive? (true for yes/false for no) ");
-//        boolean bn = sc.nextBoolean();
-//        if (bn == true) {
-//            System.out.println("Lets go for a drive");
-//        } else if (bn == false) {
-//            System.out.println("Come back when you want to go for a drive");
-//        }
         String answer;
         boolean yn;
 
@@ -27,7 +19,7 @@ public class Main {
             answer = input.nextLine().trim().toLowerCase();
             if (answer.equals("y")) {
                 yn = true;
-                System.out.println("Lets go for a drive");
+                System.out.println("Lets go for a drive!");
                 break;
             } else if (answer.equals("n")) {
                 yn = false;
@@ -43,20 +35,21 @@ public class Main {
         String destination = input.next();
         System.out.println("The directions to: " + destination + ", have been set, let's go!");
 
+        System.out.println("Your car is: " + fob.getLocked());
+
         option.gasLow();
+
         String[][] gasPrice = {
                 {"$1.50", "gets you: ", "1 Gallon"},
                 {"$3.50", "gets you: ", "2 Gallons"},
                 {"$5.50", "gets you: ", "3 Gallons"},
                 {"$7.50", "gets you: ", "4 Gallons"}};
+
         option.gasPrice(gasPrice);
 
         option.tankFilled();
 
         limit.speedLimit();
-
-        System.out.println("Your car is: " + fob.getLock() + " Your car is: " + fob.getUnlock());
-
 
         }
     }
